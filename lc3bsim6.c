ffbab962e4451b750124987be015b471cc86f299
Allison Grabowski <allisongrabowski@utexas.edu>
2015-12-04 22:52:56 -0600

    Update lc3bsim6.c

--- lc3bsim6.c
+++ lc3bsim6.c
@@ -1871,7 +1871,14 @@ void FETCH_stage() {
   {
   	/*need to check if should be oldPC + 2 or PC + 2*/
   	NEW_PS.DE_V = DEVal;
-  	NEW_PS.DE_NPC = pcmux_res;
+  	if(DEVal==0)
+  	{
+  		NEW_PS.DE_NPC = PS.DE_NPC;	
+  	}
+  	else
+  	{
+  		NEW_PS.DE_NPC = pcmux_res;	
+  	}
   	NEW_PS.DE_IR = instr;
   	
   }
